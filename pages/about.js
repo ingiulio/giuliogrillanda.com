e9b5cc8d18c85a6e0887ebb284ca5698fbf2b5f3
Giulio Grillanda <giulio.grillanda@gmail.com>
2019-02-08 10:38:51 -0800

    first

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    <p>This is, or shall I say wil be, my about page</p>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about" >
+      <button>About Page</button>
+    </Link>
+    <p>Ciao! Welcome to Giulio's personal website</p>
+  </div>
+)
+
+export default Index
